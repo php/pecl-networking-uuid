2373ab9c89b6fffdc729cd1ef8633c720a4e4e79
Hartmut Holzgraefe <hholzgra@php.net>
2004-03-30 18:10:06 +0000

    mail address change

--- uuid.c
+++ uuid.c
@@ -12,7 +12,7 @@
   | obtain it through the world-wide-web, please send a note to          |
   | license@php.net so we can mail you a copy immediately.               |
   +----------------------------------------------------------------------+
-  | Author: HArtmut Holzgraefe <hartmut@six.de>                          |
+  | Author: Hartmut Holzgraefe <hartmut@php.net>                         |
   +----------------------------------------------------------------------+
 
   $Id$ 
