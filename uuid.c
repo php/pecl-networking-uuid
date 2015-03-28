a734a82fa05ae92652563953672b2b6b73864539
Remi Collet <remi@php.net>
2015-03-28 07:11:13 +0000

    cleanup old info from CVS

--- uuid.c
+++ uuid.c
@@ -138,7 +138,6 @@ PHP_MINFO_FUNCTION(uuid)
 	php_info_print_table_header(2, "UUID extension", "enabled");
 	php_info_print_table_row(2, "Version", PHP_UUID_VERSION " (stable)");
 	php_info_print_table_row(2, "Released", PHP_UUID_RELEASED);
-	php_info_print_table_row(2, "CVS Revision", "$Id$");
 	php_info_print_table_row(2, "Authors", "Hartmut Holzgraefe 'hartmut@php.net' (lead)\n");
 	php_info_print_table_end();
 	/* add your stuff here */
