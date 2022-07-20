801b35bc1776d7a5abe5307030c0e21b3ee4597c
Remi Collet <remi@php.net>
2022-07-20 14:22:07 +0200

    constants in stub

--- uuid.stub.php
+++ uuid.stub.php
@@ -2,8 +2,110 @@
 
 /**
  * @generate-function-entries
+ * @generate-class-entries
  * @generate-legacy-arginfo
  */
+
+#ifdef UUID_VARIANT_NCS
+/**
+ * @var int
+ * @cvalue UUID_VARIANT_NCS
+ */
+const UUID_VARIANT_NCS = UNKNOWN;
+#endif
+
+#ifdef UUID_VARIANT_DCE
+/**
+ * @var int
+ * @cvalue UUID_VARIANT_DCE
+ */
+const UUID_VARIANT_DCE = UNKNOWN;
+#endif
+
+#ifdef UUID_VARIANT_MICROSOFT
+/**
+ * @var int
+ * @cvalue UUID_VARIANT_MICROSOFT
+ */
+const UUID_VARIANT_MICROSOFT = UNKNOWN;
+#endif
+
+#ifdef UUID_VARIANT_OTHER
+/**
+ * @var int
+ * @cvalue UUID_VARIANT_OTHER
+ */
+const UUID_VARIANT_OTHER = UNKNOWN;
+#endif
+
+/**
+ * @var int
+ */
+const UUID_TYPE_DEFAULT = 0;
+
+/* ---------- deprecated alias ---------- */
+
+/**
+ * @var int
+ * @cvalue UUID_TYPE_DCE
+ */
+const UUID_TYPE_DCE = UNKNOWN;
+
+/**
+ * @var int
+ * @cvalue UUID_TYPE_NAME
+ */
+const UUID_TYPE_NAME = UNKNOWN;
+
+/* ---------- library types ---------- */
+
+/**
+ * @var int
+ * @cvalue UUID_TYPE_TIME
+ */
+const UUID_TYPE_TIME = UNKNOWN;
+
+#ifdef UUID_TYPE_DCE_SECURITY
+/**
+ * @var int
+ * @cvalue UUID_TYPE_DCE_SECURITY
+ */
+const UUID_TYPE_SECURITY = UNKNOWN;
+#endif
+
+#ifdef UUID_TYPE_DCE_MD5
+/**
+ * @var int
+ * @cvalue UUID_TYPE_DCE_MD5
+ */
+const UUID_TYPE_MD5 = UNKNOWN;
+#endif
+
+/**
+ * @var int
+ * @cvalue UUID_TYPE_DCE_RANDOM
+ */
+const UUID_TYPE_RANDOM = UNKNOWN;
+
+#ifdef UUID_TYPE_DCE_SHA1
+/**
+ * @var int
+ * @cvalue UUID_TYPE_DCE_SHA1
+ */
+const UUID_TYPE_SHA1 = UNKNOWN;
+#endif
+
+/* ---------- extension specific types ---------- */
+
+/**
+ * @var int
+ */
+const UUID_TYPE_NULL = -1;
+
+/**
+ * @var int
+ */
+const UUID_TYPE_INVALID = -42;
 
 function uuid_create(int $uuid_type=UUID_TYPE_DEFAULT): string {}
 
